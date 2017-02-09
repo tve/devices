459d49675c869e5741a1e9a7938fac123477818e
Thorsten von Eicken <tve@users.noreply.github.com>
2017-02-09 14:39:34 -0800

    Create doc.go

---
+++ doc.go
@@ -0,0 +1,4 @@
+// github.com/tve/devices contains a collection of device drivers for hardware attached to gpio pins, SPI buses and I2C buses.
+// It uses google/periph for the low level access to the hardware pins. Each device driver is in its own directory and is
+// stand-alone. Simple commands to test the device can be found in the cmd directory tree.
+package devices
